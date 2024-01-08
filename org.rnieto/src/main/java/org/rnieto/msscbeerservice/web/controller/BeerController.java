ee166ccf03f1edf7a354e4b017498e1dda186778
= <=>
2024-01-08 12:07:22 +0100

    Adding API endpoints for class: 'BeerController' Http methods added: - Get - Post - Put - Delete

---
+++ org.rnieto/src/main/java/org/rnieto/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,38 @@
+package org.rnieto.msscbeerservice.web.controller;
+
+import org.rnieto.msscbeerservice.web.model.BeerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("{/beerId}")
+    public ResponseEntity<BeerDTO> getBeerDTO(@PathVariable("beerId")UUID beerId) {
+       //todo implement get entity
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDTO> saveNewBeer(@RequestBody BeerDTO beerDTO) {
+        //todo implement get entity
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("{/beerId}")
+    public ResponseEntity<BeerDTO> saveUpdatedBeer(@PathVariable("beerId")UUID beerId, @RequestBody BeerDTO beerDTO) {
+        //todo implement get entity
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
+    @DeleteMapping("{/beerId}")
+    public ResponseEntity<BeerDTO> deleteBeerDTO(@PathVariable("beerId")UUID beerId) {
+        //todo implement get entity
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    }
+}
