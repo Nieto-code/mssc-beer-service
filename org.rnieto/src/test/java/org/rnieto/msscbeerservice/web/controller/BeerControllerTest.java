979d576b66176ec9a2079166aee0e79cebe53013
= <=>
2024-03-04 11:41:06 +0100

    Adding Corrections to tests

--- org.rnieto/src/test/java/org/rnieto/msscbeerservice/web/controller/BeerControllerTest.java
+++ org.rnieto/src/test/java/org/rnieto/msscbeerservice/web/controller/BeerControllerTest.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.rnieto.msscbeerservice.web.model.BeerDTO;
+import org.rnieto.msscbeerservice.web.model.BeerStyleEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,7 +34,7 @@ class BeerControllerTest {
     @Test
     void saveNewBeer() throws Exception {
 
-        BeerDTO beer = BeerDTO.builder().build();
+        BeerDTO beer = getValidBeerDTO();
         String beerToJson = objectMapper.writeValueAsString(beer);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/beer/")
@@ -44,7 +46,7 @@ class BeerControllerTest {
     @Test
     void saveUpdatedBeer() throws Exception {
 
-        BeerDTO beer = BeerDTO.builder().build();
+        BeerDTO beer = getValidBeerDTO();
         String beerToJson = objectMapper.writeValueAsString(beer);
 
         mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/beer/"+ UUID.randomUUID().toString())
@@ -53,7 +55,12 @@ class BeerControllerTest {
                  .andExpect(status().isNoContent());
     }
 
-    @Test
-    void deleteBeerDTO() {
+    BeerDTO getValidBeerDTO(){
+        return BeerDTO.builder()
+                .beerName("My beer")
+                .beerStyle(BeerStyleEnum.IPA)
+                .price(new BigDecimal("2.00"))
+                .upc(123456789L)
+                .build();
     }
 }
